69d9ff49f83f93b06de99f7c0a21ea1f7ef5da64
Eduardo Serrano <eduardoserrano8591@gmail.com>
2022-01-21 22:09:24 -0500

    create the users model with hashed password

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  validates :username, presence: true
+  validates :username, length: {in: 3..15}
+  validates :username, uniqueness: true
+  has_secure_password
+end
