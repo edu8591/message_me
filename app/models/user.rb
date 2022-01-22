d3cf09e875f8b490e2d28184a1b48dc4aa4c40e1
Eduardo Serrano <eduardoserrano8591@gmail.com>
2022-01-21 22:22:27 -0500

    creates the message model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :messages
+
   validates :username, presence: true
   validates :username, length: {in: 3..15}
   validates :username, uniqueness: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+user = User.create!(username:"user1", password:"password")
+Message.create!(body:"This is the body for the first message", user:user)
+
+user = User.create!(username:"user2", password:"password")
+Message.create!(body:"This is the body for the second message", user:user)
+
+user = User.create!(username:"user3", password:"password")
+Message.create!(body:"This is the body for the third message", user:user)
+
+user = User.create!(username:"user4", password:"password")
+Message.create!(body:"This is the body for the fourth message", user:user)
