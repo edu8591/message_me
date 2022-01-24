3099a31f92fcb9b69d5ffeed457500c686da10ec
Eduardo Serrano <eduardoserrano8591@gmail.com>
2022-01-24 17:28:52 -0500

    added sessions create functionality and helper function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,19 @@
 class SessionsController < ApplicationController
-  def new
-  end
+	def new; end
 
-  def create
-  end
+	def create
+		user = User.find_by(useraname: params[:session][:username].downcase)
+		if user && user.authenticate(params[:session][:password])
+			session[:user_id] = user.id
+			redirect_to user
+		else
+			render new
+		end
+	end
+
+	private
+
+	def sessions_params
+		params.require(:session).permit(:username, :password)
+	end
 end
