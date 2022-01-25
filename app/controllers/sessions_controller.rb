60b390146599c944c97196bd6a97912cf4a3b835
Eduardo Serrano <eduardoserrano8591@gmail.com>
2022-01-25 00:15:01 -0500

    add flash message to sessions#create creates destroy action, updates menu partial to show links depending on logged_in?, adds delete route for sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,13 +2,22 @@ class SessionsController < ApplicationController
 	def new; end
 
 	def create
-		user = User.find_by(useraname: params[:session][:username].downcase)
+		user = User.find_by(username: params[:session][:username].downcase)
 		if user && user.authenticate(params[:session][:password])
 			session[:user_id] = user.id
-			redirect_to user
+			flash[:success] = 'You have successfully logged in'
+			redirect_to root_path
 		else
-			render new
+			flash.now[:error] =
+				'There was something wrong with your login information'
+			render :new
 		end
+	end
+
+	def destroy
+		session[:user_id] = nil
+		flash[:success] = 'You have successfully logged out'
+		redirect_to login_path
 	end
 
 	private
