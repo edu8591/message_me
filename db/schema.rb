6421e6154d0d96a7983d5639eb7d6676bcef15b0
Eduardo Serrano <eduardoserrano8591@gmail.com>
2022-03-01 00:14:40 -0500

    Changed config to use postgresql instead of sqlite

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,12 @@
 
 ActiveRecord::Schema.define(version: 2022_01_22_031120) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "messages", force: :cascade do |t|
     t.text "body"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_messages_on_user_id"
